cc9bc1daa93a1d24579d6f69880664d949a1b239
Mr Ripper <736234860@qq.com>
2016-11-04 00:26:53 +0800

    完成初始化所有配置

---
+++ application/views/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>aaa</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <base href="<?php echo site_url();?>">
+    <script>
+        window.addEventListener("load",function () {
+            setTimeout(hideURLbar,0);
+        },false);
+        function hideURLbar() {
+            window.scrollTo(0,1);
+        }
+    </script>
+</head>
+<body>
+<script src="js/jquery-1.12.4.min.js"></script>
+<script>
+    $(function () {
+
+    });
+
+</script>
+</body>
+</html>
